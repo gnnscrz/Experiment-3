8390d1a0a22e28d7e0308f3ca3574ad018983209
gnnscrz <47804393+gnnscrz@users.noreply.github.com>
2019-05-21 21:29:18 +0800

    Add files via upload

---
+++ Exp 3.3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <conio.h>
+#include <string> 
+
+using namespace std;
+
+int main()
+{
+
+ char str[50], temp;
+
+    int i, j, cnt=0;
+
+    cout << "Enter an array : ";
+    gets(str);
+
+    j = strlen(str) - 1;
+
+    for (i = 0; i < j; i++,j--)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;	
+	}
+	cout << "\nReversed array: " << str << endl << endl; 
+
+
+	for (int i = 0; str [i]; i++)
+	{
+		cnt++; 
+	}
+
+	cout << "Size of Array: " << cnt << endl; 
+
+_getch();
+return 0; 
+}
